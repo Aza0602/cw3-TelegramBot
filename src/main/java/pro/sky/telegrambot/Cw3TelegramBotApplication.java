63e380d10b8065269b1e9ddfde51da300ce9ed11
Azat <aelchikushuulu@gmail.com>
2023-06-18 18:32:38 +0600

    Initial commit

---
+++ src/main/java/pro/sky/telegrambot/Cw3TelegramBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cw3TelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cw3TelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/telegrambot/Cw3TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cw3TelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
